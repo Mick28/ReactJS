8e04d7eaf1b5869ea38c94f55e55daa532c4aa86
Mick28 <miguelecurra@gmail.com>
2025-03-31 17:00:56 -0300

    Update funcion anonima

--- Class01/hola-mundo/src/components/ComponenteH1.jsx
+++ Class01/hola-mundo/src/components/ComponenteH1.jsx
@@ -1,3 +1,5 @@
+const Bienvenida = () => <h1>Desde React.</h1>;
+
 function ComponenteH1() 
 {
     return(
@@ -5,4 +7,5 @@ function ComponenteH1()
         )
 }
 
-export default ComponenteH1;
+// export default ComponenteH1;
+export default Bienvenida;
